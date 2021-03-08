93bfa829e81bde644cc613b472c8f83e5f71a9c9
Alexis Lecomte <alexis.lecomte@supinfo.com>
2021-03-08 16:26:35 +0100

    Initial Commit

---
+++ Covid Racer/Covid_RacerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid_RacerApp.swift
+//  Covid Racer
+//
+//  Created by Spoon Overlord on 08/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Covid_RacerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
